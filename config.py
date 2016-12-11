4d037ad54b13b8d24188158aaa60edfd5f4b1958
Dan Busbridge <dan.busbridge@gmail.com>
2016-12-11 15:56:18 +0000

    Ignore saved networks

--- config.py
+++ config.py
@@ -4,16 +4,16 @@ class AgentConfig(object):
     replay_memory = 10
     batch_size = 5
     gamma = 0.99
-    explore_start = 50000
-    learn_start = 100000
-    max_step = 200000
+    explore_start = 100000
+    learn_start = 500000
+    max_step = 2000000
     initial_epsilon = 1
     final_epsilon = 0.001
     keep_prob_config = 0.95
     network_choice = 'DeepMind'
-    restore_from_saved_model = False
+    restore_from_saved_model = True
     model_dir = 'saved_models/Breakout-v0'
-    model_restore_steps = 20000
+    model_restore_steps = 400000
     steps_save = 100000
 
 
--- train_model.py
+++ train_model.py
@@ -2,6 +2,9 @@ import importlib as il
 import tensorflow as tf
 import agent.agent as agent
 import game.game as game
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 sess = tf.InteractiveSession()
 
